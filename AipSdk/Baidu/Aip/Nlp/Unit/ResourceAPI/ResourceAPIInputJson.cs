d4b84b80f2a122a8e5be093bce39848070e4ceff
朱峰 <tq01azk@gmail.com>
2018-10-22 15:58:07 +0800

    ResourceAPI添加相关JSON

---
+++ AipSdk/Baidu/Aip/Nlp/Unit/ResourceAPI/ResourceAPIInputJson.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Baidu.Aip.Nlp.Unit.ResourceAPI
+{
+    public class ResourceAPIInputJson
+    {
+        /// <summary>
+        /// 0为正常，其他为错误
+        /// </summary>
+        public string error_code { get; set; }
+        /// <summary>
+        /// 	错误描述
+        /// </summary>
+        public string error_msg { get; set; }
+
+        /// <summary>
+        /// 对象
+        /// </summary>
+        public ResourceAPIResult result { get; set; }
+    }
+
+    public class ResourceAPIResult
+    {
+        /// <summary>
+        /// 现在仅支持纯文本返回text，其他预留
+        /// </summary>
+        public string type = "text";
+        /// <summary>
+        /// 当返回是text类型时，content为返回话术
+        /// </summary>
+        public string content { get; set; }
+    }
+}
---
+++ AipSdk/Baidu/Aip/Nlp/Unit/ResourceAPI/ResourceAPIReturnJson.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Baidu.Aip.Nlp.Unit.ResourceAPI
+{
+    public class ResourceAPICallBackResultJSON : Result
+    {
+        /// <summary>
+        ///from参数代表展示平台，其中
+        //1 为DuerOS技能开放平台；
+        //2 为微信公众平台。
+        /// </summary>
+        public string form { get; set; }
+    }
+}
